374a84f51b7a83233c0d7f22df04662ba0556ecf
bsaxe <bjoernsaxe@gmx.de>
2017-11-07 11:09:47 +0100

    PrimeFaces-GUI entfernt

--- isy-benutzerverwaltung-gui-primefaces/src/main/java/de/bund/bva/isyfact/benutzerverwaltung/gui/common/konstanten/KonfigurationsSchluessel.java
+++
@@ -1,9 +0,0 @@
-package de.bund.bva.isyfact.benutzerverwaltung.gui.common.konstanten;
-
-/**
- * @author Björn Saxe, msg systems ag
- */
-public abstract class KonfigurationsSchluessel {
-
-    public static final String REDIRECT_URL_NACH_LOGIN = "benutzerverwaltung.gui.redirecturl.nachlogin";
-}
